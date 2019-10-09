7ff1de69d4653cae76a03a600327dbf82ef42c26
Ji Beom <wgb1223@gmail.com>
2019-10-09 21:19:36 +0900

    Add files via upload

---
+++ CameraController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Gamja;
+    void Update()
+    {
+        if (Gamja.transform.position.y > transform.position.y + 5) transform.position = new Vector3(0, Gamja.transform.position.y - 5, -10);
+    }
+}
